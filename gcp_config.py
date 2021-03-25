78426a97fb90b2514ada6acc5f180bc407267d73
Mohamed Laradji <mlaradji@protonmail.ch>
2021-03-25 12:20:35 -0700

    WIP: rebaseme

--- gcp_config.py
+++ gcp_config.py
@@ -1,7 +1,7 @@
 JOB_CONFIG = {
     # "JOB_NAME": "gcp_test_2", # A name should start with a letter and contain only letters, numbers and underscores.
     "PACKAGE_PATH": "./src/",
-    "MODULE_NAME": "trainval.trainval",
+    "MODULE_NAME": "trainval",
     "JOB_DIR": "gs://haven-ai-gcp-test-1/data",
     "REGION": "us-west1",
     "PROJECT_ID": "haven-ai-gcp-308300",
